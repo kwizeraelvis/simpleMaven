65c687329b2fc4dad92817d1c6f66b83e24b22f1
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2019-12-13 19:02:37 +0200

    Git Actions test

--- src/main/java/com/elvis/mavenproject2/Main.java
+++ src/main/java/com/elvis/mavenproject2/Main.java
@@ -12,5 +12,6 @@ package com.elvis.mavenproject2;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Maven");
+        System.out.println("The beginig of a new journey");
     }
 }
